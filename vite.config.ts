837ae871f80e6513c24adf4d8460bb507aeca8fb
mhmae <mhmae@unsitra.fr>
2026-01-15 15:12:09 +0100

    🔧 Add vite-plugin-vue-devtools & adapt vite config and cleanup default content

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig, loadEnv } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import { fileURLToPath, URL } from "node:url"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools({
+        launchEditor: env.VITE_EDITOR || "webstorm",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "#": fileURLToPath(new URL("./types", import.meta.url)),
+        "~": fileURLToPath(new URL("./tests", import.meta.url)),
+        "&": fileURLToPath(new URL("./constants", import.meta.url)),
+      },
+    },
+  }
+})
